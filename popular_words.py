ae4214273a868b15b1b63fe2c9116aa05bb10ee9
Olexandr Zhytenko <zhytenko@ukr.net>
2018-04-25 11:50:33 +0300

    popular_words modified

--- popular_words.py
+++ popular_words.py
@@ -5,6 +5,16 @@ def popular_words(text, words):
             dict_words[i.lower()] += 1
     return dict_words
 
+# def popular_words(text, words):
+#     lower_count = text.lower().split().count
+#     return {word: lower_count(word) for word in words}
+
+# def popular_words(text, words):
+#     from collections import Counter
+#     import re
+#     count = Counter(re.split("[^a-z']+", text.lower()))
+#     return {word: count[word] for word in words}
+
 if __name__ == "__main__":
     text = '''When I was One
               I had just begun
